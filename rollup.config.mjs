1b249cd2b307e6f7ff783c925f0c3bbf508d4676
Mark Christian Fajardo <cfajardo25@gmail.com>
2023-09-19 16:18:46 +0800

    Add storybook package.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -12,6 +12,7 @@ import packageJson from './package.json' assert { type: 'json' };
 export default [
   {
     input: 'src/index.ts',
+    external: ['react-dom'],
     output: [
       {
         file: packageJson.main,
